3f889e36df42a0bafac3df76065a3082179017cf
Maria Amanda <mariaaraujo.amanda@gmail.com>
2021-09-21 22:22:08 -0300

    criamos a classe Usuario

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php 
 
-require_once("config.php");
-
-$sql = new Sql();
+/*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
+
+require_once("config.php");
+
+$root = new Usuario();
+
+$root->loadById(3);
+
+echo $root;
 
  ?>
