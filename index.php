1b0e4c8b4ad74cd8167bc0f367315f071fdb29f4
Maria Amanda <mariaaraujo.amanda@gmail.com>
2021-09-26 18:16:45 -0300

    comando delete

--- index.php
+++ index.php
@@ -28,6 +28,8 @@ $aluno->insert();
 echo $aluno;
 */
 
+/*
+//Alterar um usuário
 $usuario = new Usuario();
 
 $usuario->loadById(8);
@@ -35,5 +37,13 @@ $usuario->loadById(8);
 $usuario->update("professor", "!@#$%¨&*");
 
 echo $usuario;
+*/
 
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
+
+echo $usuario;
 ?>
